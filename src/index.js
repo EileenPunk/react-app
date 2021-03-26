9e4221fe56b897b2a9074d8db53416841b014a88
Eileen Punk <eileenpunk@EileenPunk-3.local>
2021-03-26 17:17:54 +0100

    react install

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import './sass/app.scss';
+
+const element = <h1>hello world</h1>
+
+ReactDOM.render(element, document.getElementById('root'));
